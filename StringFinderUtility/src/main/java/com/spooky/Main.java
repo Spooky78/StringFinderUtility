db533fb0a3eaa02c0dd9bde2d664c194ef9938fe
Spooky <vic78dark@gmail.com>
2025-09-11 20:51:52 +0100

    Coded determineSearchRegex

--- StringFinderUtility/src/main/java/com/spooky/Main.java
+++ StringFinderUtility/src/main/java/com/spooky/Main.java
@@ -1,65 +1,106 @@
 package com.spooky;
 
-import org.apache.commons.cli.*;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
 
+/**
+ * Main Class.
+ */
 public class Main {
-    public static void main(String[] args) {
-        try {
-            run(args);
-        } catch (ParseException e) {
-            System.err.println("Error parsing command line arguments " + e.getMessage());
-        }
+
+  /**
+   * Main method that runs the program.
+   *
+   * @param args Program arguments
+   */
+  public static void main(String[] args) {
+    run(args);
+  }
+
+  private static void run(String[] args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+      Options options = buildOptions();
+
+      CommandLineParser commandLineParser = new DefaultParser();
+      CommandLine commandLine = commandLineParser.parse(options, args);
+
+      String searchSpecification = determineSearchRegex(commandLine, scanner);
+      Path dirToSearchPath = determineSearchDirectory(commandLine, scanner);
+    } catch (Exception e) {
+      System.err.println("ERROR: Something went wrong");
+    }
+  }
+
+  private static Options buildOptions() {
+    Options options = new Options();
+    options.addOption(createOption("d", "dir", "DIR", "Directory To Search", false));
+    options.addOption(createOption("r", "regex", "REGEX", "Regex To Search For", false));
+    return options;
+  }
+
+  private static Path determineSearchDirectory(CommandLine commandLine, Scanner scanner) {
+    String dirToSearch;
+    if (commandLine.hasOption("d")) {
+      dirToSearch = commandLine.getOptionValue("d");
+
+    } else {
+      System.out.println("No directory was given to search, please give one:");
+      if (scanner.hasNextLine()) {
+        dirToSearch = scanner.nextLine().trim();
+      } else {
+        throw new IllegalArgumentException("No directory input provided");
+      }
     }
 
-    static void run(String[] args) throws ParseException {
-        Options options = new Options();
-        Option dirToSearchArg = createOption("d", "dir", "DIR", "Directory To Search", false);
-        options.addOption(dirToSearchArg);
+    Path path = Paths.get(dirToSearch);
 
-        CommandLineParser commandLineParser = new DefaultParser();
-        CommandLine commandLine = commandLineParser.parse(options, args);
-
-        Path dirToSearchPath = determineSearchDirectory(commandLine, dirToSearchArg);
+    if (!Files.exists(path)) {
+      throw new IllegalArgumentException("Invalid directory: " + dirToSearch);
     }
 
-    static Path determineSearchDirectory(CommandLine commandLine, Option dirToSearchArg) {
-        String dirToSearch;
-        if (commandLine.hasOption("d")) {
-            dirToSearch = commandLine.getOptionValue(dirToSearchArg);
+    return path;
+  }
 
-        } else {
-            System.out.println("No directory was given to search, please give one:");
-            try (Scanner scanner = new Scanner(System.in)) {
-                dirToSearch = scanner.nextLine().trim();
-            }
-        }
+  private static String determineSearchRegex(CommandLine commandLine, Scanner scanner) {
+    String regexToSearch;
+    if (commandLine.hasOption("r")) {
+      regexToSearch = commandLine.getOptionValue("r");
 
-        Path path = Paths.get(dirToSearch);
-
-        if (!Files.exists(path)) {
-            throw new IllegalArgumentException("Invalid directory: " + dirToSearch);
-        }
-
-        return path;
+    } else {
+      System.out.println("No regex was given to search, please give one:");
+      if (scanner.hasNextLine()) {
+        regexToSearch = scanner.nextLine().trim();
+      } else {
+        throw new IllegalArgumentException("No regex input provided");
+      }
     }
 
-    public static SearchSpecification determineSearch(CommandLine cmd, Option dirOption) {
-        return null;
+    try {
+      java.util.regex.Pattern.compile(regexToSearch);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Invalid regular expression: " + regexToSearch);
     }
 
-    static Option createOption(String shortName, String longName, String argName, String description, boolean required) {
-        return Option.builder(shortName)
-                .longOpt(longName)
-                .argName(argName)
-                .desc(description)
-                .hasArg()
-                .required(required)
-                .build();
-    }
+    return regexToSearch;
+  }
+
+
+  private static Option createOption(String shortName, String longName, String argName,
+                                     String description, boolean required) {
+    return Option.builder(shortName)
+        .longOpt(longName)
+        .argName(argName)
+        .desc(description)
+        .hasArg()
+        .required(required)
+        .build();
+  }
 
 }
